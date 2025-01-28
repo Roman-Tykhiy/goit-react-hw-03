27eea7f6becaa299bebbd077ff82be47631c0eb5
Roman_Tykhiy <2rstykhiy@gmail.com>
2025-01-28 16:07:26 +0200

    hw33-1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,38 @@
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
 const ContactForm = ({ handleSubmit}) => {
     const initialValues = {
         name: "",
         phone: "",
     };
+
+    const onlyLaters = /^[A-Za-zА-Яа-яЇїІіЄєҐґ'’\s]+$/;
+    const phoneValidation = /^\+?\d{9,15}$/;
+     const applySchema = Yup.object().shape({
+    name: Yup.string()
+      .required("поле обов'язкове")
+      .min(3, "мінімум 3 символи")
+      .max(20, "максимум 20 символів")
+      .matches(onlyLaters, "введіть літери!"),
+    phone: Yup.string()
+      .matches(phoneValidation, "Невірний формат номера телефону")
+      .required("поле обов'язкове"),
+  });
     
     return (
         <div>
-            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={applySchema}>
                 <Form>
                     <Field name="name"></Field>
+                    <ErrorMessage 
+              name="name"
+              component="p"
+            />
                     <Field name="phone"></Field>
+                     <ErrorMessage
+              name="phone"
+              component="p"
+            />
                     <button type="submit">Add contact</button>
                 </Form>
             </Formik>
